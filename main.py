9cc8d130e9fe41a03679baa8644b49ebbe379d9e
brandrey5 <brandonrey2005@gmail.com>
2025-11-06 14:24:00 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+# Author: Brandon Reyes-Perez
+# Date Created: 10/30/2025
+# Last Modified: 11/06/2025
+# Reference: https://www.codedex.io/projects/build-a-word-guessing-game-with-python
+
+import random
+import time
+
+WORD_BANK = ['plug', 'import', 'word', 'teeth']
+
+hangmanStages = ['''
+    +---+
+    |   |
+        |
+        |
+        |
+    ======
+''', '''
+    +---+
+    |   |
+    O   |
+        |
+        |
+    ======
+''', '''
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+    ======
+''', '''
+    +---+
+    |   |
+    O   |
+    |\  |
+        |
+    ======
+''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+    ======
+''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+     \  |
+    ======
+''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+   / \  |
+    ======
+''']
+
+word = random.choice(WORD_BANK)
+
+guessedWord = ['_'] * len(word) # a list
+
+attempts = 7
+curr = 0 # index for which hangman stage to use
+
+while attempts > 0:
+    print('\nCurrent word: ' + ' '.join(guessedWord))# print contents of list
+    guess = input('Guess a letter: ').lower()
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                guessedWord[i] = guess
+            print('Great guess!')
+    else:
+        attempts -= 1
+        print(hangmanStages[curr])
+        curr += 1
+        print('Wrong guess! Attempts left: ' + str(attempts))
+
+    if '_' not in guessedWord:
+        print('\nCongratulations!! You guessed the word')
+        break
+
+    if attempts == 0 and '_' in guessedWord:
+        print('\nYou ran out of attempts! The word was: ' + word)
